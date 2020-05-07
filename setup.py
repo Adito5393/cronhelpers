019f74d7c0e93326ee96ded2ea3cf91960e73e1b
Adi <adrianionlazar@yahoo.com>
2020-05-07 19:10:14 +0200

    added changes to compile for Python 3.5;

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.util import convert_path
 from typing import Any, Dict
 
 # https://stackoverflow.com/a/24517154/4575168
-main_ns: Dict[str, Any] = {}
+main_ns = {}
 ver_path = convert_path('cronhelpers/version.py')
 with open(ver_path) as ver_file:
     exec(ver_file.read(), main_ns)
@@ -23,6 +23,7 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8"
     ],
